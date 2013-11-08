98c13bcce0d01b95ea803af71661ca95d4ec783c
Psylynce <justin.powell@colorado.edu>
2013-11-08 15:19:28 -0800

    Scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HAPPyHOURr::Application.config.secret_token = '07c211468c1653e836ff98c3a65b16d28857fe5bdf79c689fd62e2fcf88ccd25c3db0da41826f0d549a34052272bf04da1f30cae1e84fa8c3a2bfd9ff4260ce7'
