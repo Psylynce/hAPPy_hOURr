744fd5a752b39868dbc25a6f80dba1768a057089
saasbook <saasbook@saasbook.(none)>
2013-12-03 22:08:47 -0800

    Done for the night, something wrong with users_controller.rb create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def create
+  	@user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+
 end
