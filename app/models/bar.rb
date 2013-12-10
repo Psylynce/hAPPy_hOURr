906146af291e87e9eb9f631660c6c95f7a9a1101
Justin Powell <justin.powell@colorado.edu>
2013-12-09 21:30:43 -0700

    Sort bars by area

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,6 @@
 class Bar < ActiveRecord::Base
-	
+	def self.all_areas
+		# ['Downtown', 'The Hill', 'South Boulder', 'North Boulder', 'East Boulder']
+		%w(Downtown The\ Hill South\ Boulder North\ Boulder East\ Boulder)
+	end
 end
