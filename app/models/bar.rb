9f0e1dd7863705fb95a11686df15c18de6d6e565
Justin Powell <justin.powell@colorado.edu>
2013-12-10 14:25:30 -0700

    Begining of a reputation system

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,4 +1,12 @@
 class Bar < ActiveRecord::Base
+	validates :name, presence: true
+
+	has_reputation :rating, source: :user, aggregated_by: :average
+
+	def initialize(attributes = nil)
+		super(attributes)
+	end
+
 	def self.all_areas
 		%w(Downtown The\ Hill South North East)
 	end
